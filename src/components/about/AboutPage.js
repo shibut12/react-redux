b5a3c66a6ba272f58f7fecf3d8f0f37d941313f6
Shibu Thannikkunnath <shibu@hotmail.co.in>
2018-08-29 00:43:16 -0400

    Created About page

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>About</h1>
+                <p>This app uses React, redux and react router, ES6 and a variety of other tools to make this ultra-responsive web apps.</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
