908e49e6023126893392e571876f135e9c830201
Shibu Thannikkunnath <shibu@hotmail.co.in>
2018-08-30 23:54:09 -0400

    Fixed typo

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,5 @@ import {combineReducers} from 'redux';
 import courses from './courseReducer';
 
 const rootReducer = combineReducers({
-    courses // short habd version, same as courses: courses
+    courses // short hand property name, same as courses: courses
 });
