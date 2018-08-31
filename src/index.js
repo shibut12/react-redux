c75f517b6ccc2217fe5e9c2494dcf9aa541a67fa
Shibu Thannikkunnath <shibu@hotmail.co.in>
2018-08-31 00:18:10 -0400

    Created  add course flow

--- src/index.js
+++ src/index.js
@@ -6,7 +6,15 @@ import routes from './routes';
 import './styles/styles.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
+
+const store = configureStore();
+
+
 render(
-    <Router history={browserHistory} routes={routes} />,
+    <Provider store={store}>
+        <Router history={browserHistory} routes={routes} />
+    </Provider>,
     document.getElementById('app')
 );
