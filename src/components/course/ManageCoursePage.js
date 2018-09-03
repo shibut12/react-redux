f037a74e7f00a8f2243ffd1e9bb5c2510ee55aaf
Shibu Thannikkunnath <shibu@hotmail.co.in>
2018-09-03 12:00:08 -0400

    Added componentWillReceiveProps to update state when props changed

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -17,6 +17,12 @@ class ManageCoursePage extends React.Component{
         this.saveCourse = this.saveCourse.bind(this);
     }
 
+    componentWillReceiveProps(nextProps){
+        if(this.props.course.id != nextProps.course.id) {
+            this.setState({course: Object.assign({}, nextProps.course)});
+        }
+    }
+
     updateCourseState(event){
         const field = event.target.name;
         let course = this.state.course;
@@ -56,8 +62,19 @@ ManageCoursePage.contextTypes = {
     router: PropTypes.object
 };
 
+function getCourseById(courses, id){
+    const course = courses.filter(course => course.id == id);
+    if(course.length) return course[0];
+    return null;
+}
+
 function mapStateToProps(state, ownProps){
+    const courseId = ownProps.params.id; // from the path /course/:id
     let course={id:'', watchRef:'', title:'', authorId:'', length:'', category:''};
+
+    if(courseId && state.courses.length > 0){
+        course = getCourseById(state.courses, courseId);
+    }
     
     const authorsFormattedForDropdown = state.authors.map(author => {
         return {
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -10,7 +10,7 @@ export default function courseReducer(state = initialState.courses, action){
                 ...state,
                 Object.assign({}, action.course)
             ];
-        case types.UPDATE_COURSES_SUCCESS:
+        case types.UPDATE_COURSE_SUCCESS:
             return [
                 ...state.filter(course => course.id !== action.course.id),
                 Object.assign({}, action.course)
