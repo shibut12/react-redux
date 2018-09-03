8a50cc95974ec3326ceedff99f380ebbf1aca07c
Shibu Thannikkunnath <shibu@hotmail.co.in>
2018-09-03 11:34:43 -0400

    Created action reducer and type and bindign for save course

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -14,6 +14,7 @@ class ManageCoursePage extends React.Component{
         };
 
         this.updateCourseState = this.updateCourseState.bind(this);
+        this.saveCourse = this.saveCourse.bind(this);
     }
 
     updateCourseState(event){
@@ -21,6 +22,11 @@ class ManageCoursePage extends React.Component{
         let course = this.state.course;
         course[field] = event.target.value;
         return this.setState({course: course});
+    }
+
+    saveCourse(event){
+        event.preventDefault();
+        this.props.actions.saveCourse(this.state.course);
     }
 
     render(){
@@ -32,6 +38,7 @@ class ManageCoursePage extends React.Component{
                     errors={this.state.errors}
                     allAuthors={this.props.authors}
                     onChange={this.updateCourseState}
+                    onSave={this.saveCourse}
                 />
             </div>
         );
@@ -40,7 +47,8 @@ class ManageCoursePage extends React.Component{
 
 ManageCoursePage.propTypes = {
     course: PropTypes.object.isRequired,
-    authors: PropTypes.array.isRequired
+    authors: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps){
